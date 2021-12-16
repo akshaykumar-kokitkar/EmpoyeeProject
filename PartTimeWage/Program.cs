b0c36f862672378dd434d52ce2b89e38c8099874
ak47 <kokitkarak@gmail.com>
2021-12-16 12:07:49 +0530

    added UC3

---
+++ PartTimeWage/Program.cs
@@ -0,0 +1,30 @@
+﻿//UC3
+class progarm
+{
+    //constants
+        public const int IS_PART_TIME = 0;
+        public const int EMP_RATE_PER_HOUR = 20;
+    static void Main(string[] args)
+    {
+        
+        //variables
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+        // Computation
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_PART_TIME)
+        {
+            empHrs = 4;
+            Console.WriteLine("Employee is part time\n  ");
+        }
+        else
+        {
+            empHrs = 8;
+            Console.WriteLine("Employee is full time \n  ");
+
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage : " + empWage);
+    }
+}
