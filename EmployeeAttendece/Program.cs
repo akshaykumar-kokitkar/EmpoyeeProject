6459110196ba043219b93dd53664ffd258f394c4
ak47 <kokitkarak@gmail.com>
2021-12-16 11:52:23 +0530

    Added UC1

---
+++ EmployeeAttendece/Program.cs
@@ -0,0 +1,20 @@
+﻿//UC1
+class Program
+{
+    //constants
+    public const int IS_FULL_TIME = 1;
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        //computation
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is Present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is Absent");
+        }
+    }
+}
